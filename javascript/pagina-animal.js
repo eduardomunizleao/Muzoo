2027492d3ed7001ce9b54de8e34f41784cc5928b
Eduardo Muniz <edu4rdomunizle4o@gmail.com>
2024-11-05 22:31:17 -0300

    hu

--- javascript/pagina-animal.js
+++ javascript/pagina-animal.js
@@ -11,11 +11,20 @@ document.querySelectorAll('.tooltip').forEach((tooltip) => {
 
 function toggleinfo() {
     const open = document.querySelector('#infomobile');
+    const opened = document.querySelector('#open')
     if (open) {
         if (open.classList.contains('active')) {
             open.classList.remove('active');
         } else {
             open.classList.add('active');
+        }
+    }
+
+    if (opened) {
+        if (opened.classList.contains('active')) {
+            opened.classList.remove('active');
+        } else {
+            opened.classList.add('active');
         }
     }
 }
